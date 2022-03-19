d2e6d84ca8a4dc7248bdc318e772fd550c077245
Jason Bacon <bacon@head.albacore>
2022-03-19 11:08:11 -0500

    Separate raw merged files and raw renamed links

--- RNA-Seq/01-organize.sh
+++ RNA-Seq/01-organize.sh
@@ -57,7 +57,7 @@ done
 ##############################################################################
 
 cd Data
-mkdir -p Raw-renamed
+mkdir -p Raw-merged Raw-renamed
 cd Raw-renamed
 sample=1
 time_step=1
@@ -65,7 +65,7 @@ for day in 0 2 4 7 12; do
     for rep in 1 2 3; do
 	for read in 1 2; do
 	    orig=$(ls ../../../Raw/BCAUAGANXX/${day}RNA${rep}_*_R${read}*.fastq.gz)
-	    merged=${day}RNA-${rep}-merged-R${read}.fastq.gz
+	    merged=../Raw-merged/${day}RNA-${rep}-merged-R${read}.fastq.gz
 	    readable=sample$sample-rep$rep-time$time_step-R$read.fastq.gz
 	    printf "$readable -> $merged\n"
 	    if [ ! -e $merged ]; then
