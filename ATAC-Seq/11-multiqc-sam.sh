00528b40ed4d708281a53fa5ce3e8096cc7a262f
Jason Bacon <bacon@head.albacore>
2022-03-25 13:15:58 -0500

    Adapt SAM QC and merge from CNC-EMDiff

---
+++ ATAC-Seq/11-multiqc-sam.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+if which srun > /dev/null; then
+    srun=srun
+else
+    srun=''
+fi
+
+# multiqc: LC_ALL and LANG must be set to a UTF-8 character set
+# in your environment in order for the click module to function.
+export LC_ALL=en_US.UTF-8
+
+$srun multiqc --version > Logs/11-multiqc-sam/multiqc-version.txt 2>&1
+
+dir=Data/11-multiqc-sam
+mkdir -p $dir/Raw $dir/Rmdup
+
+cd $dir/Raw
+$srun multiqc ../../10-qc-sam/Raw
+
+cd ../Rmdup
+$srun multiqc ../../10-qc-sam/Rmdup
